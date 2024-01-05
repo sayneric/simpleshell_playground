17e7ae96f593899c264786f903dbcf6c9fb99228
sayneric <saynerical@gmail.com>
2024-01-05 02:36:01 -0800

    handled the env built-in task

--- run_task.c
+++ run_task.c
@@ -3,13 +3,15 @@
 
 #define MAX_COMMAND_LENGTH 100
 
+
 /**
- * accomplish_task - Execute  specified command if it exists and is executable.
+ * accomplish_task - Execute specified command if it exists and is executable.
  * @command: The command to execute.
  *
- * This function checks if the specified command exists and is executable.
- * If the command is found, it is executed; otherwise, error message is printed
- * and the program exits with a failure status.
+ * This function checks if the specified command is a built-in command.
+ * If it is a built-in command, it executes the corresponding function;
+ * otherwise, it checks if the command exists and is executable.
+ * If the command is found, it is executed; otherwise, an error message is printed.
  */
 
 void accomplish_task(const char *command)
@@ -17,7 +19,12 @@ void accomplish_task(const char *command)
 	char command_path[MAX_COMMAND_LENGTH];
 
 	/* check if command exists and is executable */
-	if (check_command_existence(command, command_path))
+	if (compare_strings(command, "env"))
+	{
+		/* Handle the 'env' builtin command */
+		env_builtin();
+	}
+	else if (check_command_existence(command, command_path))
 	{
 		/* Execute command */
 		execute_command(command_path, command);
